bb960fcda5a71abe1611cff2518f7487322b00f7
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-05-30 15:50:08 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contacts, setContacts] = useState([]);
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (name.trim() === "" || phoneNumber.trim() === "") {
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name: name.trim(),
+      phoneNumber: phoneNumber.trim(),
+    };
+
+    setContacts([...contacts, newContact]);
+    setName("");
+    setPhoneNumber("");
+  };
 
   return (
-    <>
+    <div>
+      <h2>Książka telefoniczna</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name"></label>
+          <input
+            type="text"
+            placeholder="imię i nazwisko"
+            id="name"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="phoneNumber"></label>
+          <input
+            type="tel"
+            placeholder="numer telefonu"
+            id="phoneNumber"
+            name="phoneNumber"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+        </div>
+        <button type="submit">Dodaj kontakt</button>
+      </form>
+      <h2>Lista kontaktów:</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {contacts.map((contact) => (
+          <span key={contact.id}>
+            {contact.name} : {contact.phoneNumber}
+          </span>
+        ))}
       </div>
-      <h1>phonebook</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
